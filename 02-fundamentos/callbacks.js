9c1ebf95eebee9a16d464df23077bcefc7abbc67
Ivan Urbano <ivanurbano@htech.com.mx>
2022-10-03 14:01:52 -0500

    Creación de callbacks

---
+++ 02-fundamentos/callbacks.js
@@ -0,0 +1,27 @@
+// Callback tradicional
+setTimeout(function(){
+    console.log('Hola Mundo')
+}, 1000);
+
+
+// Callback con función de flecha
+setTimeout(() =>{
+    console.log('Hola Mundo 2')
+}, 3000);
+
+
+//
+const getUsuarioByID = (id, callback) => {
+    const user = {
+        id,
+        nombre: 'Ivancho'
+    }
+    setTimeout(()=> {
+        callback(user)
+    }, 1500)
+}
+
+getUsuarioByID(10, (usuario) => {
+    console.log(usuario.id)
+    console.log(usuario.nombre.toUpperCase())
+});
